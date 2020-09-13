33e58fd9c9443358f91f365fa6b19a8a0ef2558c
Abdullah06444 <abdullah44gur@gmail.com>
2020-09-13 09:01:41 +0300

    initial commit

---
+++ CSharp0015_Arrays.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CSharp0015_Arrays
+{
+
+    class CSharp0015_Arrays
+    {
+        static void Main(string[] args)
+        {
+            
+            int size = 1, i = -1, n = 0, k = 0;
+            int[] array = { };
+            string s = Console.ReadLine();
+            var input = s.Split(' ');
+            foreach(var number in input)
+            {
+                if (i == -1) {
+                    size = int.Parse(number);
+                    array = new int[size];
+                }
+                else if (i < size)
+                    array[i] = int.Parse(number);
+                else if (i == size)
+                    n = int.Parse(number);
+                else
+                    k = int.Parse(number);
+                i++;
+            }
+            
+            rotate(array, n, k);
+        }
+
+        private static void rotate(int[] a, int n, int k)
+        {
+            int i = 0;
+
+            foreach (int item in a)
+            {
+                if (i < n)
+                    Console.Write(a[(i + n - k) % n] + " ");
+                else
+                    Console.Write(a[i] + " ");
+                i++;
+            }
+        }
+    }
+}
